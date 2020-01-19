269aa686360dbf5367f0ee5386ff5f7be951a490
RossGoodliffe <rossgoodliffe@gmail.com>
2020-01-19 19:26:47 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,34 +1,26 @@
-
 //Smooth Scroll 
 const scroll = new SmoothScroll('#navbar a[href*="#"]', {
     speed: 800
 });
 
 
-
-//Display text on Landing
-displayText = () => {
-    document.getElementById("hidden").style.display = "block";
-
-}
-
-//Fade Text in when arriving
+//Fade in Subtitle and Social img  
 fadeIn = () => {
-    //document.getElementById("navbar").style.opacity = "1";
-    document.getElementById("hidden").style.opacity = "1";
+    document.getElementById("hidden-title").style.opacity = "1";
     document.getElementById("social").style.opacity = "1";
 }
 
-//run fadeIn after 4 seconds
-setTimeout(fadeIn,4000);
+//run fadeIn after 1 second
+setTimeout(fadeIn,1000);
 
-//display relevant greeting based on current time
+
+//Display relevant greeting based on current time
 displayGreeting = () => {
     let today = new Date();
     let currentTime = today.getHours();
     let greeting;
 
-    if (currentTime > 18) {
+    if (currentTime > 17) {
         greeting = "Good Evening!";
     } else if (currentTime > 12) {
     greeting = "Good Afternoon!"
@@ -38,17 +30,10 @@ displayGreeting = () => {
     greeting = "Hello!"
     }
 
-    let about = document.getElementById("about-head").innerHTML = greeting + " My name is Ross Goodliffe";
+    let about = document.getElementById("greeting").innerHTML = greeting + " My name is Ross Goodliffe";
 }
 
 //run greeting function on page load
-window.onload = displayGreeting;
+window.onload = displayGreeting();
 
-//thank you for submitting form
-thankYou = () => {
-    document.getElementById("contact").innerHTML = "<p>Thank you for getting in contact! I will be in touch shortly</p>";
-
-}
-
-
-
+// window.onload;
